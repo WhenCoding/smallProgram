645ae12d8bafa74ed2ecc5ff09a21b2182385665
xgs <mynamecoder@163.com>
2018-05-02 15:13:58 +0800

    update

---
+++ demo/diaries.js
@@ -0,0 +1,147 @@
+var diaries = [
+  {
+    meta: {  // 内容元数据
+      cover: "http://m.chanyouji.cn/index-cover/64695-2679221.jpg?imageView2/1/w/620/h/330/format/jpg",
+      avatar: "/images/my/rabbit.jpg",
+      title: "此刻静好，愿幸福长存",
+      meta: "2018.04.29",
+      create_time: "2018.04.29 11:57:27",
+      nickName: "Ruby",
+    },
+    list: [
+      {
+        type: "TEXT",
+        content: '4月29日，今天是灰色的一天，是送行的一天。因工作需要突然的离别让我们本来短暂的温存更加显得弥足珍贵。而且今天我们还大吵了一架。',
+        poi: {
+          longitude: 112.2,
+          latitude: 34.4,
+          name: "洛阳市",
+        },
+        description: "河科大",
+        id: 1,
+        commentNum: 0,
+        likeNum: 0,
+      },
+      {
+          type: "IMAGE",
+          content: 'http://p.chanyouji.cn/1473699595/1740E45C-D5AF-497E-A351-06E5BA22B1A3.jpg',
+          poi: {
+            longitude: 112.2,
+            latitude: 34.4,
+            name: "泉舜海底捞",
+          },
+          description: "在这里，我们一起吃的火锅。你和我很是开心。开心地都忘记了之前还大吵了一顿。你还问我：你说不要我了？傻瓜。都是气话。",
+          id: 2,
+          commentNum: 1,
+          likeNum: 5,
+      },
+      {
+        type: "IMAGE",
+        content: 'http://p.chanyouji.cn/1473699603/7C3B253F-0A31-4754-B042-E04115F2C931.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "泉舜",
+        },
+        description: "我们为了能够及时吃上火锅，叫了号之后一起逛了两个店。",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+      {
+        type: "TEXT",
+        content: '大早上我们一番风雨之后，你来了冷冷的冰雨。我说：下周我可以再来。',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "悦途",
+        },
+        description: "",
+        id: 1,
+        commentNum: 0,
+        likeNum: 0,
+      },
+      {
+        type: "IMAGE",
+        content: 'http://p.chanyouji.cn/1473685830/2A48B40F-1F11-498D-ABD2-B0EDCD09F776.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "洛阳理工门口烧烤",
+        },
+        description: "那晚我们撸串。喝着汉斯小木屋。雪花。玩着成语接龙。还有逢七过，以为自己能赢。然并卵。",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+      {
+        type: "VIDEO",
+        content: 'http://flv.bn.netease.com/videolib3/1605/22/auDfZ8781/HD/auDfZ8781-mobile.mp4',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "琴湖",
+        },
+        description: "那晚我们完成了属于我们的梦。",
+        id: 2,
+        commentNum: 10,
+        likeNum: 200,
+      },
+    ]
+  },
+  {
+    meta: {  // 内容元数据
+      cover: "http://m.chanyouji.cn/articles/625/ca9e50f74e273041e3a399bf5528f7b5.jpg?imageView2/1/w/620/h/330/format/jpg",
+      avatar: "/images/my/rabbit.jpg",
+      title: "我们当然有属于我们的未来",
+      meta: "2018.4.28",
+      create_time: "2018.04.28 11:57:27",
+      nickName: "Ruby",
+    },
+    list: [
+      {
+        type: "TEXT",
+        content: '2018年五一，心情急切的我，还没听到法定节假日的钟声，就来到了洛阳--有你的城市。宛若落叶归根。\n',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "北京朝阳",
+        },
+        description: "",
+        id: 1,
+        commentNum: 0,
+        likeNum: 0,
+      },
+      {
+        type: "TEXT",
+        content: '第一天 天气：格外晴\n\n短短四个小时的高铁，让我感觉从所未有的漫长。 \n像往常一样坐上了28路，并且像往常一样下错了站。你满口埋怨，在我听来仍旧是满满的幸福。看到微信定位显示你我之间的距离在慢慢缩短的感觉真好。 \n天，蓝，云，白。洛阳的空气格外清新。马路对面骑着租八戒的你今天格外美丽。如天上的云，时而像珍珠点点，时而像棉絮团团。\n。',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "洛阳学子街爱转角",
+        },
+        description: "",
+        id: 1,
+        commentNum: 0,
+        likeNum: 0,
+      },
+      {
+        type: "IMAGE",
+        content: 'http://p.chanyouji.cn/64695/1377177446705p182j2oa9031j1p0b5vpuvj1voj2.jpg-o',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "河南科技大学",
+        },
+        description: "乖。这两天我们很幸福，但还是没有缺少增进感情的机会。你说我有些事情没给你说，可是你知道嘛，你赚了，朋友能解决的事情，我从来就不会麻烦你，再累也不能累到我女朋友。你说你喜欢冯雪鸽这种女生，可是你知道嘛，我们男生都喜欢你这种，不管别人怎么想，反正我就喜欢你这种性格的。再说王者荣耀，我们群里的小伙伴的热情你也早已体会，你知道嘛，我的意识和操作用666都不能充分表达，再加上我们多年的好友总是推脱也不好，所以我和海镇陪他们上分，实属无奈，尽请谅解。 \n 俗话说：打江山容易，守江山难。两个人走到最后真的很不容易。So,please believe me,you are my today and all of my tomorrows.",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+    ]
+  }
+]
+
+module.exports = {
+  diaries: diaries,
+}
